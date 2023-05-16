e1dbffca7802a7fae4e71258cf3b9f3bd0a003ce
Denis <denis.makouski@bseu.by>
2023-05-16 16:05:51 +0300

    version ++

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   =  2023051501;
+$plugin->version   =  2023051600;
 $plugin->requires  =  2016112900;
 $plugin->component =  'block_needtodo';
 
