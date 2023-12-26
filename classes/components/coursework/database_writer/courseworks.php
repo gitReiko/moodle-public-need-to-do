62cdfa914f9d16c31c5cba3b28bdcec2a4b2b106
Denis <denis.makouski@bseu.by>
2023-12-26 11:08:40 +0300

    unreaded messages

--- classes/components/coursework/database_writer/courseworks.php
+++ classes/components/coursework/database_writer/courseworks.php
@@ -16,23 +16,20 @@ class Coursework
     {
         $this->outdatedTimestamp = $outdatedTimestamp;
 
-        $courseworks = $this->get_sent_for_check_courseworks();
-        $courseworks = $this->filter_out_ready_courseworks($courseworks);
+        $unchecked = $this->get_sent_for_check_courseworks();
+        $unchecked = $this->filter_out_ready_courseworks($unchecked);
 
-        
+        $unreaded = $this->get_unreaded_messages();
+        $unreaded = $this->filter_out_non_teacher_messages($unreaded);
 
 
-        print_r($courseworks);
+        print_r($unchecked);
+        echo '<hr>';
+        print_r($unreaded);
 
         //echo '<hr>'.$this->outdatedTimestamp;
 
 
-
-        // only unchecked
-
-        // get sent for check after date
-
-        // is work not ready
 
         // add to array
 
@@ -116,6 +113,67 @@ class Coursework
 
         return !$DB->record_exists_sql($sql, $params);
     }
+
+    /**
+     * Returns unreaded coursework messages.
+     * 
+     * @return array unreaded messages
+     */
+    private function get_unreaded_messages() : ?array 
+    {
+        global $DB;
+
+        $sql = 'SELECT id, coursework, userto, sendtime 
+                FROM {coursework_chat} 
+                WHERE readed = ?
+                AND sendtime > ?';
+        $params = array(0, $this->outdatedTimestamp);
+
+        return $DB->get_records_sql($sql, $params);
+    }
+
+    /**
+     * Returns unreaded teachers chat messages.
+     * 
+     * @param array unreaded messages
+     * 
+     * @param array unreaded teachers messages
+     */
+    private function filter_out_non_teacher_messages(?array $unreaded) : ?array 
+    {
+        $teachers = array();
+
+        foreach($unreaded as $message)
+        {
+            if($this->is_receiver_teacher($message))
+            {
+                $teachers[] = $message;
+            }
+        }
+
+        return $teachers;
+    }
+
+    /**
+     * Returns true if receiver it's teacher.
+     * 
+     * @param stdClass message 
+     * 
+     * @return bool 
+     */
+    private function is_receiver_teacher(\stdClass $message) : bool 
+    {
+        global $DB;
+
+        $where = array(
+            'coursework' => $message->coursework,
+            'teacher' => $message->userto
+        );
+
+        return $DB->record_exists('coursework_teachers', $where);
+    }
+
+
 
 
 
